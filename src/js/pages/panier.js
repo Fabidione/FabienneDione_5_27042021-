ef919a7a468932fff09bffc2b3bfdd13f4c32161
Fabienne Dione <fabiennedione@gmail.com>
2021-06-05 02:35:49 +0000

    ajout panier

--- src/js/pages/panier.js
+++ src/js/pages/panier.js
@@ -6,4 +6,35 @@ document.addEventListener("DOMContentLoaded", function () {
   //declarer une variable dans laquelle on met les keys et les values//convertir json en js avec parse
   let produitenregistre = JSON.parse(localStorage.getItem("produit"));
   console.log(produitenregistre);
+
+  //----------------------------affichage des produits du panier------------//
+  // selection de la claisse où je vais injecter le code html//
+  const positionelement = document.querySelector("#container-produits-panier");
+  console.log(positionelement);
+
+  let structureproduitpanier = [];
+
+  //si le panier est vide//
+  if (produitenregistre === null) {
+    const paniervide = `
+  <div class="container-panier-vide">
+    <div> Le panier est vide </div>
+  </div>`;
+    document.getElementById("container-produits-panier").innerHTML = paniervide;
+  } else {
+    //si le panier n'est pas vide//
+    for (let k = 0; k < produitenregistre.length; k++) {
+      structureproduitpanier =
+        structureproduitpanier +
+        `
+      <div class="container-recap">
+        <div class="article">${produitenregistre[k].nomproduit}</div>
+        <div class="quantite">${produitenregistre[k].quantiteproduit}</div>
+        <div class="prixunit">${produitenregistre[k].prixproduit / 100} €</div>
+        <div>Supprimer l'article</div>
+      </div>`;
+      document.getElementById("container-produits-panier").innerHTML =
+        structureproduitpanier;
+    }
+  }
 });
